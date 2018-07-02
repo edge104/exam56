14bf6a7317ad7764652f14f487ebd3884f7dd825
edge <68104@ms23.url.com.tw>
2018-07-02 16:01:58 +0800

    註冊系統、中文化、樣板練習，改首頁樣板

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//基本動作：傳參數給view﹙視圖﹚
+Route::get('/', function () {
+    $name = 'edge';
+    $say  = 'Hello!';
+    return view('welcome', compact('name', 'say'));
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
