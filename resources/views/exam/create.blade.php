505d57dbc46db704bcff26175316e46296da76a0
edge <68104@ms23.url.com.tw>
2018-07-03 15:58:33 +0800

    安裝marvinlabs/laravel-html-bootstrap-4套件

--- resources/views/exam/create.blade.php
+++ resources/views/exam/create.blade.php
@@ -1,1 +1,14 @@
-86888
+<!--繼承主樣板-->
+@extends('layouts.app')
+
+<!--塞值給主樣板裡的content scetion-->
+@section('content')
+    <h1>{{ __('Create Exam') }}</h1>
+
+    {{ bs()->openForm('post', '/exam') }}
+        {{ bs()->text('測驗標題')->placeholder('請填入測驗標題') }}
+        {{ bs()->select('enable', ['1' => '開啟', '0' => '關閉'], '1') }}
+
+    {{ bs()->closeForm() }}    
+
+@endsection
